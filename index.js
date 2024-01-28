41a8872373de013c49ace17fd6e41eff0ad5505b
NTL <64105932+ntl-mix@users.noreply.github.com>
2024-01-28 18:28:53 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = 'your token here'
-const id = 'chat id here'
+const token = '6651217766:AAHLSUIMMMxNYh979c5libdHIe3D7F21BZk'
+const id = '6642385061'
 const address = 'https://www.google.com'
 
 const app = express();
